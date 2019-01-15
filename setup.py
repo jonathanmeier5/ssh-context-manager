6f4d7c872ae4a377bd1692e115e87ed97a8ce139
Jonathan <meierj@Jonathans-MacBook-Pro.local>
2019-01-14 23:10:08 -0500

    First pass at setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+
+setup(
+    name='ssh-context-manager',
+    url='https://github.com/jonathanmeier5/ssh-context-manager',
+    version='0.0.0',
+    author='jonathan meier',
+    author_email='jonathan.w.meier@gmail.com',
+    package_dir={'': 'src'},
+    packages=['*'],
+)
